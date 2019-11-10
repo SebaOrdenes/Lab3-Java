717d3ee3bf8cc592426af557a2950dc97c3094f4
Sebastián Órdenes <sebaordenes1996@gmail.com>
2019-11-09 21:55:16 -0300

    segundo commit, de prueba, creacion de un package con las funciones

---
+++ src/funciones/Rfuncionales.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funciones;
+
+/**
+ *
+ * @author acer
+ */
+public class Rfuncionales {
+    public void createScene(int N, int M, int numEnemies, int dificultad, int seed ){
+       
+       
+   }
+}
